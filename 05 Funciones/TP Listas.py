f5c63fc432ad40947f5062e7423c630a048ca650
Leandro Lopez <leandrolopez.06@gmail.com>
2025-05-08 13:33:37 -0300

    se realiza entrega trabajo practico de listas

---
+++ 05 Funciones/TP Listas.py
@@ -0,0 +1,51 @@
+#Ejercicio 1
+multiplo_4 = list(range(0,101,4))
+print(multiplo_4)
+
+#Ejercicio 2
+elementos= [1,2,3,4,5]
+print(elementos[3])
+
+#Ejercicio 3
+lista_vacia=[]
+lista_vacia.append(1)
+lista_vacia.append(3)
+lista_vacia.append(8)
+print(lista_vacia)
+
+#Ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1]= "loro"
+animales[3]= "oso"
+print(animales)
+
+#Ejercicio 5
+#La funcion del programa es crear una lista llamada con 5 elementos y remover el elemento mas alto de la lista luego imprimir dicha lista por pantalla.
+
+#Ejercicio 6
+numeros=list(range(10,31,5))
+print(numeros[0:2])
+
+#Ejercicio 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1]= "bora"
+autos[2]= "camaro"
+print(autos)
+
+#Ejercicio 8
+dobles=[]
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+
+#Ejercicio 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1]= "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#Ejercicio 10
+lista_anidada=[15,True,[25.5,57.9,30.6],False]
+print(lista_anidada)
